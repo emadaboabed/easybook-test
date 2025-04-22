fad9007f9a699029dc8b300645cd5c9fa50c6e23
emadaboabeed <emad0956543124@gmail.com>
2025-04-22 14:47:23 +0300

    fix: errors with deployments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 import "devextreme/dist/css/dx.light.css";
 
 export default function Home() {
-  redirect("http://localhost:3000/login");
+  redirect("/login");
 }
